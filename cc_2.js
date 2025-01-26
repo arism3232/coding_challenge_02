2bcefbe5a387c18b871f008928d1cd53cf210f4f
Aritree Hoque <aritree.hoque@gmail.com>
2025-01-25 22:29:24 -0500

    Task1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+// Task 1 - Working with Arrays
+let products = ["Lip Balm", "Hand Cream", "Mints", "Gum", "Hand Sanitizer"]; // Array of products
+products.push("Floss"); // Adding new product at the end of array
+products.pop(); // Removing last product from the end of array
+console.log("Updated product list:", products);
